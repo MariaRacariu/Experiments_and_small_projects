47508c0cb6ffe00756b58975a5ab14a9b8399d52
MariaRacariu <90383391+MariaRacariu@users.noreply.github.com>
2022-02-21 13:29:32 +0100

    Create Getters&Setters.php
    added variables and some functions

---
+++ Getters&Setters.php
@@ -0,0 +1,23 @@
+<?php
+
+class User{
+    public $username;
+    private $email;
+    private $password;
+
+    public function __construct($username, $email, $password)
+    {
+        $this->username = $username;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+}
+
+$userOne = new User('Maria', 'maria@email.com', 'Admin123');
+$userTwo = new User('Rasmus', 'rasmus@email.com', 'Password');
+
+echo $userOne->getEmail();
